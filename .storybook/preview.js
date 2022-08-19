164cf8997d717c78646dc57b10d6cb5d5dee641d
Jake Dluhy <jake@fuzzy.com>
2022-08-19 16:25:50 -0700

    Update preview

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,13 @@
+import * as NextImage from 'next/image';
+
+// Storybook doesn't recognize the `next/image` image optimization. So we turn it off just for storybook.
+const OriginalNextImage = NextImage.default;
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />
+});
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +17,8 @@ export const parameters = {
     },
   },
 }
+
+// The above fix, on its own, needs this addition to account for a breaking update:
+NextImage.defaultProps = {
+  unoptimized: true,
+};
