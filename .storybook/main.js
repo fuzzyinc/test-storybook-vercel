0895bcf1d5eed0666ff91a0ebc0dd6248dc6a41f
Jake Dluhy <jake@fuzzy.com>
2022-08-30 12:14:13 -0700

    Attempt to ignore middleware from storybook build

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const webpack = require('webpack');
+
 module.exports = {
   "stories": [
     "../stories/**/*.stories.mdx",
@@ -11,5 +13,17 @@ module.exports = {
   "framework": "@storybook/react",
   "core": {
     "builder": "@storybook/builder-webpack5"
+  },
+  webpackFinal: (config) => {
+    config.plugins.forEach((plugin, idx) => {
+      if (plugin instanceof webpack.IgnorePlugin) {
+        const originalExcludes = plugin.options.resourceRegExp.source;
+        plugin.options.resourceRegExp = new RegExp(`${originalExcludes}|.*middleware\.ts`)
+      }
+    });
+
+    console.log(config.plugins);
+
+    return config;
   }
 }
